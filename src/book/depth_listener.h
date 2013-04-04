15009ca823292d0eff5274e9afc6c97b0e79f66f
Jeff Schmitz <jeffrey.j.schmitz@gmail.com>
2013-04-04 08:45:24 -0500

    Listener for depth events.

---
+++ src/book/depth_listener.h
@@ -0,0 +1,22 @@
+// Copyright (c) 2012, 2013 Object Computing, Inc.
+// All rights reserved.
+// See the file license.txt for licensing information.
+#ifndef depth_listener_h
+#define depth_listener_h
+
+#include "order_book.h"
+
+namespace liquibook { namespace book {
+
+/// @brief listener of depth events
+template <class OrderBook, class DepthTracker >
+class DepthListener {
+public:
+  /// @brief callback for change in tracked aggregated depth
+  virtual void on_depth_change(const OrderBook& book, 
+                               const DepthTracker& depth) = 0;
+};
+
+} }
+
+#endif
