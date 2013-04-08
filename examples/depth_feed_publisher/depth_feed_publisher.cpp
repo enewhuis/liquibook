c4e85c1ec3d640c0a07363927eef77a8be075fc1
Jeff Schmitz <jeffrey.j.schmitz@gmail.com>
2013-04-08 09:41:53 -0500

    Create an exchange in example

--- examples/depth_feed_publisher/depth_feed_publisher.cpp
+++ examples/depth_feed_publisher/depth_feed_publisher.cpp
@@ -2,10 +2,14 @@
 #include <iostream>
 #include "exchange.h"
 
+using namespace liquibook;
+
 int main(int argc, const char* argv[])
 {
   std::cout << "hello" << std::endl;
   // Create exchange
+  examples::Exchange exchange;
+
   // Populate with securities
   // Create feed publisher
   // Generate orders
