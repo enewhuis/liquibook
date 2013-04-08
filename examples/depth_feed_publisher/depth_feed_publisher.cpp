658e82d0d5336410438e475f3da466750c198873
Jeff Schmitz <jeffrey.j.schmitz@gmail.com>
2013-04-08 14:55:30 -0500

    Updates

---
+++ examples/depth_feed_publisher/depth_feed_publisher.cpp
@@ -0,0 +1,16 @@
+
+#include "depth_feed_publisher.h"
+
+namespace liquibook { namespace examples { 
+
+DepthFeedPublisher::DepthFeedPublisher()
+{
+}
+
+void
+DepthFeedPublisher::on_depth_change(const ExampleOrderBook* order_book,
+                                    const Tracker* tracker)
+{
+}
+
+} } // End namespace
