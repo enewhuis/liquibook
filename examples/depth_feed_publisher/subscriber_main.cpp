d16a7e33108242d6be7b42c7155006633e032469
Jeff Schmitz <jeffrey.j.schmitz@gmail.com>
2013-05-21 16:10:46 -0500

    Comments

--- examples/depth_feed_publisher/subscriber_main.cpp
+++ examples/depth_feed_publisher/subscriber_main.cpp
@@ -11,7 +11,7 @@ int main(int argc, const char* argv[])
   // Create feed subscriber
   liquibook::examples::DepthFeedSubscriber feed(connection.get_templates());
 
-  // Connect to server
+  // Set up handlers
   liquibook::examples::MessageHandler msg_handler =
       boost::bind(&liquibook::examples::DepthFeedSubscriber::handle_message,
                   &feed, _1, _2);
@@ -20,6 +20,8 @@ int main(int argc, const char* argv[])
                   &feed);
   connection.set_message_handler(msg_handler);
   connection.set_reset_handler(reset_handler);
+
+  // Connect to server
   connection.connect();
   connection.run();
 
