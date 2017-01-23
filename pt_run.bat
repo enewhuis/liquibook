d5ceefd361195a4b90b1cefded6e49f85960b4f8
Dale Wilson <wilsond@ociweb.com>
2017-01-23 15:44:48 -0600

    Batch files to run tests.

---
+++ pt_run.bat
@@ -0,0 +1,1 @@
+test\pt_order_book.exe
---
+++ ut_run.bat
@@ -0,0 +1,7 @@
+test\ut_all_or_none.exe
+test\ut_bbo_order_book.exe
+test\ut_depth.exe
+test\ut_immediate_or_cancel.exe
+test\ut_listeners.exe
+test\ut_order_book.exe
+test\ut_order_book_shared_ptr.exe
