403f98b0f43e969701b04c230d32b6e1a262c2fe
Dale Wilson <wilsond@ociweb.com>
2017-02-01 08:45:50 -0600

    Move OrderTracker to its own file.

--- src/book/order_book.h
+++ src/book/order_book.h
@@ -1,9 +1,9 @@
 // Copyright (c) 2012, 2013 Object Computing, Inc.
 // All rights reserved.
 // See the file license.txt for licensing information.
-#ifndef order_book_h
-#define order_book_h
+#pragma once
 
+#include "order_tracker.h"
 #include "callback.h"
 #include "order.h"
 #include "order_listener.h"
@@ -27,48 +27,6 @@ class OrderListener;
 template<class OrderBook>
 class OrderBookListener;
 
-/// @brief Tracker of an order's state, to keep inside the OrderBook.  
-///   Kept separate from the order itself.
-template <class OrderPtr = Order*>
-class OrderTracker {
-public:
-  /// @brief construct
-  OrderTracker(const OrderPtr& order, OrderConditions conditions = 0);
-
-  /// @brief modify the order quantity
-  void change_qty(int32_t delta);
-
-  /// @brief fill an order
-  /// @param qty the number of shares filled in this fill
-  void fill(Quantity qty); 
-
-  /// @brief is there no remaining open quantity in this order?
-  bool filled() const;
-
-  /// @brief get the total filled quantity of this order
-  Quantity filled_qty() const;
-
-  /// @brief get the open quantity of this order
-  Quantity open_qty() const;
-
-  /// @brief get the order pointer
-  const OrderPtr& ptr() const;
-
-  /// @brief get the order pointer
-  OrderPtr& ptr();
-
-  /// @ brief is this order marked all or none?
-  bool all_or_none() const;
-
-  /// @ brief is this order marked immediate or cancel?
-  bool immediate_or_cancel() const;
-
-private:
-  OrderPtr order_;
-  Quantity open_qty_;
-  OrderConditions conditions_;
-};
-
 /// @brief The limit order book of a security.  Template implementation allows
 ///        user to supply common or smart pointers, and to provide a different
 ///        Order class completely (as long as interface is obeyed).
@@ -82,8 +40,10 @@ public:
   typedef TradeListener<MyClass > TypedTradeListener;
   typedef OrderBookListener<MyClass > TypedOrderBookListener;
   typedef std::vector<TypedCallback > Callbacks;
-  typedef std::multimap<Price, Tracker, std::greater<Price> >  Bids;
-  typedef std::multimap<Price, Tracker, std::less<Price> >     Asks;
+  typedef std::greater<Price> BidComparison;
+  typedef std::less<Price> AskComparison;
+  typedef std::multimap<Price, Tracker, BidComparison >  Bids;
+  typedef std::multimap<Price, Tracker, AskComparison >     Asks;
   typedef std::list<typename Bids::iterator> DeferredBidCrosses;
   typedef std::list<typename Asks::iterator> DeferredAskCrosses;
 
@@ -210,90 +170,6 @@ private:
   Price sort_price(const OrderPtr& order);
   bool add_order(Tracker& order_tracker, Price order_price);
 };
-
-template <class OrderPtr>
-inline
-OrderTracker<OrderPtr>::OrderTracker(
-  const OrderPtr& order,
-  OrderConditions conditions)
-: order_(order),
-  open_qty_(order->order_qty()),
-  conditions_(conditions)
-{
-}
-
-template <class OrderPtr>
-inline void
-OrderTracker<OrderPtr>::change_qty(int32_t delta)
-{
-  if ((delta < 0 && 
-      (int)open_qty_ < std::abs(delta))) {
-    throw 
-        std::runtime_error("Replace size reduction larger than open quantity");
-  }
-  open_qty_ += delta;
-}
-
-template <class OrderPtr>
-inline void
-OrderTracker<OrderPtr>::fill(Quantity qty) 
-{
-  if (qty > open_qty_) {
-    throw std::runtime_error("Fill size larger than open quantity");
-  }
-  open_qty_ -= qty;
-}
-
-template <class OrderPtr>
-inline bool
-OrderTracker<OrderPtr>::filled() const
-{
-  return open_qty_ == 0;
-}
-
-template <class OrderPtr>
-inline Quantity
-OrderTracker<OrderPtr>::filled_qty() const
-{
-  return order_->order_qty() - open_qty();
-}
-
-template <class OrderPtr>
-inline Quantity
-OrderTracker<OrderPtr>::open_qty() const
-{
-  return open_qty_;
-}
-
-template <class OrderPtr>
-inline const OrderPtr&
-OrderTracker<OrderPtr>::ptr() const
-{
-  return order_;
-}
-
-template <class OrderPtr>
-inline OrderPtr&
-OrderTracker<OrderPtr>::ptr()
-{
-  return order_;
-}
-
-template <class OrderPtr>
-inline bool
-OrderTracker<OrderPtr>::all_or_none() const
-{
-  // TODO: Consider making this a property of the order rather than keeping external flags.
-  return bool(conditions_ & oc_all_or_none);
-}
-
-template <class OrderPtr>
-inline bool
-OrderTracker<OrderPtr>::immediate_or_cancel() const
-{
-    // TODO: Consider making this a property of the order rather than keeping external flags.
-    return bool((conditions_ & oc_immediate_or_cancel) != 0);
-}
 
 template <class OrderPtr>
 OrderBook<OrderPtr>::OrderBook(const std::string & symbol)
@@ -924,4 +800,3 @@ OrderBook<OrderPtr>::matches(
 
 } }
 
-#endif
---
+++ src/book/order_tracker.h
@@ -0,0 +1,136 @@
+// Copyright (c) 2012, 2013 Object Computing, Inc.
+// All rights reserved.
+// See the file license.txt for licensing information.
+#pragma once
+
+#include "types.h"
+
+namespace liquibook { namespace book {
+
+/// @brief Tracker of an order's state, to keep inside the OrderBook.  
+///   Kept separate from the order itself.
+template <class OrderPtr = Order*>
+class OrderTracker {
+public:
+  /// @brief construct
+  OrderTracker(const OrderPtr& order, OrderConditions conditions = 0);
+
+  /// @brief modify the order quantity
+  void change_qty(int32_t delta);
+
+  /// @brief fill an order
+  /// @param qty the number of shares filled in this fill
+  void fill(Quantity qty); 
+
+  /// @brief is there no remaining open quantity in this order?
+  bool filled() const;
+
+  /// @brief get the total filled quantity of this order
+  Quantity filled_qty() const;
+
+  /// @brief get the open quantity of this order
+  Quantity open_qty() const;
+
+  /// @brief get the order pointer
+  const OrderPtr& ptr() const;
+
+  /// @brief get the order pointer
+  OrderPtr& ptr();
+
+  /// @ brief is this order marked all or none?
+  bool all_or_none() const;
+
+  /// @ brief is this order marked immediate or cancel?
+  bool immediate_or_cancel() const;
+
+private:
+  OrderPtr order_;
+  Quantity open_qty_;
+  OrderConditions conditions_;
+};
+
+template <class OrderPtr>
+inline
+OrderTracker<OrderPtr>::OrderTracker(
+  const OrderPtr& order,
+  OrderConditions conditions)
+: order_(order),
+  open_qty_(order->order_qty()),
+  conditions_(conditions)
+{
+}
+
+template <class OrderPtr>
+inline void
+OrderTracker<OrderPtr>::change_qty(int32_t delta)
+{
+  if ((delta < 0 && 
+      (int)open_qty_ < std::abs(delta))) {
+    throw 
+        std::runtime_error("Replace size reduction larger than open quantity");
+  }
+  open_qty_ += delta;
+}
+
+template <class OrderPtr>
+inline void
+OrderTracker<OrderPtr>::fill(Quantity qty) 
+{
+  if (qty > open_qty_) {
+    throw std::runtime_error("Fill size larger than open quantity");
+  }
+  open_qty_ -= qty;
+}
+
+template <class OrderPtr>
+inline bool
+OrderTracker<OrderPtr>::filled() const
+{
+  return open_qty_ == 0;
+}
+
+template <class OrderPtr>
+inline Quantity
+OrderTracker<OrderPtr>::filled_qty() const
+{
+  return order_->order_qty() - open_qty();
+}
+
+template <class OrderPtr>
+inline Quantity
+OrderTracker<OrderPtr>::open_qty() const
+{
+  return open_qty_;
+}
+
+template <class OrderPtr>
+inline const OrderPtr&
+OrderTracker<OrderPtr>::ptr() const
+{
+  return order_;
+}
+
+template <class OrderPtr>
+inline OrderPtr&
+OrderTracker<OrderPtr>::ptr()
+{
+  return order_;
+}
+
+template <class OrderPtr>
+inline bool
+OrderTracker<OrderPtr>::all_or_none() const
+{
+  // TODO: Consider making this a property of the order rather than keeping external flags.
+  return bool(conditions_ & oc_all_or_none);
+}
+
+template <class OrderPtr>
+inline bool
+OrderTracker<OrderPtr>::immediate_or_cancel() const
+{
+    // TODO: Consider making this a property of the order rather than keeping external flags.
+    return bool((conditions_ & oc_immediate_or_cancel) != 0);
+}
+
+} }
