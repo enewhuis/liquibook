3adf28ee6f66200ca4bd1bc44f5a6863f0b0e670
Dale Wilson <wilsond@ociweb.com>
2017-02-06 14:11:53 -0600

    Run the market price unit test from ut_run

--- ut_run.bat
+++ ut_run.bat
@@ -1,7 +1,8 @@
+test\ut_order_book.exe
+test\ut_market_price.exe
 test\ut_all_or_none.exe
 test\ut_bbo_order_book.exe
 test\ut_depth.exe
 test\ut_immediate_or_cancel.exe
 test\ut_listeners.exe
-test\ut_order_book.exe
 test\ut_order_book_shared_ptr.exe
