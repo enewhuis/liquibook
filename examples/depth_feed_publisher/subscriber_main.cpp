5139348ebe18f064b98c2415197970e1ed669a01
owais-kali <sh.owais66@gmail.com>
2023-04-21 07:57:55 +0530

    Compile Error Fixed!

--- examples/depth_feed_publisher/subscriber_main.cpp
+++ examples/depth_feed_publisher/subscriber_main.cpp
@@ -1,12 +1,10 @@
 
-#include <boost/bind.hpp>
 #include "depth_feed_connection.h"
 #include "depth_feed_subscriber.h"
+#include <boost/bind.hpp>
 
-int main(int argc, const char* argv[])
-{
-  try
-  {
+int main(int argc, const char *argv[]) {
+  try {
     // Create the connection
     liquibook::examples::DepthFeedConnection connection(argc, argv);
 
@@ -14,21 +12,17 @@ int main(int argc, const char* argv[])
     liquibook::examples::DepthFeedSubscriber feed(connection.get_templates());
 
     // Set up handlers
-    liquibook::examples::MessageHandler msg_handler =
-        boost::bind(&liquibook::examples::DepthFeedSubscriber::handle_message,
-                    &feed, _1, _2);
-    liquibook::examples::ResetHandler reset_handler =
-        boost::bind(&liquibook::examples::DepthFeedSubscriber::handle_reset,
-                    &feed);
+    liquibook::examples::MessageHandler msg_handler = boost::bind(
+        &liquibook::examples::DepthFeedSubscriber::handle_message,&feed, _1, _2);
+    liquibook::examples::ResetHandler reset_handler = boost::bind(
+        &liquibook::examples::DepthFeedSubscriber::handle_reset, &feed);
     connection.set_message_handler(msg_handler);
     connection.set_reset_handler(reset_handler);
 
     // Connect to server
     connection.connect();
     connection.run();
-  }
-  catch (const std::exception & ex)
-  {
+  } catch (const std::exception &ex) {
     std::cerr << "Exception caught at main level: " << ex.what() << std::endl;
     return -1;
   }
